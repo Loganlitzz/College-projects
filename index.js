7d72f0480b78ab58a80de8cd225d06b152a377a0
Loganlitzz <76871632+Loganlitzz@users.noreply.github.com>
2022-04-06 08:32:49 +0530

    Add files via upload
    this update contains fixed javascript code and some flex tweakings ;-)

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-console.log("Hello");
-
-a = document.getElementById("submit");
-document.write(a);
+document.getElementById("discription-view").innerHTML=("Hello sample text");
+document.getElementById("submit").addEventListener("click",function() {
+    var disc = document.getElementById("discription").value;
+    document.getElementById("discription-view").innerHTML=disc;
+    var blob =new Blob([data],{ type: "text/plain;charset=utf-8"});
+    const filename= 'litzz.txt';
+    let newLink = document.createElement("a");
+        newLink.download = filename;
+        
+});
 
